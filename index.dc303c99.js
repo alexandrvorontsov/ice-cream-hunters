ad0bb0fa47e03ebac1f697c2e96103f460140ce7
SkillFix <107311906+SkillFix@users.noreply.github.com>
2022-08-09 10:34:51 +0000

    Deploying to gh-pages from @ alexandrvorontsov/ice-cream-hunters@875f31e033ce75cdb928d830f36afd0cd7e41774 🚀

---
+++ index.dc303c99.js
@@ -0,0 +1,2 @@
+function playSound(e){var o=document.getElementById(e);o.volume=1,o.paused?o.play():o.pause()}!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("body")};function o(){e.modal.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.dc303c99.js.map
